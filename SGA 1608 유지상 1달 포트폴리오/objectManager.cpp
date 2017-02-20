dce7feedb5065e57c7d4e3a5c01fc7e9a08680ee
YOO JI SANG <digndign2@naver.com>
2017-02-20 12:03:18 +0900

    아이템 좌표 이동 함수가 생성되었습니다.

--- SGA 1608 유지상 1달 포트폴리오/objectManager.cpp
+++ SGA 1608 유지상 1달 포트폴리오/objectManager.cpp
@@ -19,6 +19,38 @@ void objectManager::render()
 {
 }
 
+void objectManager::setHeart(float x, float y)
+{
+	item* _item;
+	_item = new heart;
+	_item->init(x, y);
+	_vItem.push_back(_item);
+}
+
+void objectManager::setShuriken(float x, float y)
+{
+	item* _item;
+	_item = new shuriken;
+	_item->init(x, y);
+	_vItem.push_back(_item);
+}
+
+void objectManager::addItemX(int value)
+{
+	for (_viItem = _vItem.begin(); _viItem != _vItem.end(); ++_viItem)
+	{
+		(*_viItem)->addItemX(value);
+	}
+}
+
+void objectManager::addItemY(int value)
+{
+	for (_viItem = _vItem.begin(); _viItem != _vItem.end(); ++_viItem)
+	{
+		(*_viItem)->addItemY(value);
+	}
+}
+
 objectManager::objectManager()
 {
 }
